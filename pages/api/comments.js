05c6f9b85da14d9b683c51ec02ae722d1c393f1b
EL JABRI <90796656+ahmed112s@users.noreply.github.com>
2022-02-01 23:00:20 +0100

    update

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -4,7 +4,7 @@ const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
 /** *************************************************************
 * Any file inside the folder pages/api is mapped to /api/* and  *
-* will be treated as an API endpoint instead of a page.         *
+* will be treated as an API endpoint instead of a page. ee        *
 *************************************************************** */
 
 // export a default function for API route to work
